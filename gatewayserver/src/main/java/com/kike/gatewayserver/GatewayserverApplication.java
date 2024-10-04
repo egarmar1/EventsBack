a5bf0a70dc9b90763ffc587b2c00703c6b117719
egarmar1 <kikegm2001@gmail.com>
2024-10-04 15:47:53 +0200

    gateway reouting done, now let's add resilience

--- gatewayserver/src/main/java/com/kike/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/kike/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,33 @@ package com.kike.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
+
+    @Bean
+    public RouteLocator fastBookRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+				.route( p -> p
+						.path("/fastbook/event/**")
+						.filters(f -> f.rewritePath("/fastbook/event/(?<segment>.*)", "/${segment}" )
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://EVENT"))
+
+                .route( p -> p
+                        .path("/fastbook/booking/**")
+                        .filters(f -> f.rewritePath("/fastbook/booking/(?<segment>.*)", "/${segment}" ))
+                        .uri("lb://BOOKING")).build();
+
+    }
 }
